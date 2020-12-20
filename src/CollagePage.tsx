7fdd11876fa6a531dcf17520c30d285d77819a21
ryu0114h <ryu0114h@gmail.com>
2020-12-20 14:14:58 +0900

    songsの追加

--- src/CollagePage.tsx
+++ src/CollagePage.tsx
@@ -9,6 +9,92 @@ import {
 import { Audio } from "expo-av";
 import Icon from "react-native-vector-icons/FontAwesome";
 
+const songs = [
+  {
+    id: ["n-15-1", "n-15-2"],
+    song: require("../assets/乃木坂/裸足でSummer.mp3"),
+  },
+  {
+    id: ["n-16-1", "n-16-2", "n-16-3"],
+    song: require("../assets/乃木坂/サヨナラの意味.mp3"),
+  },
+  {
+    id: ["n-18-1", "n-18-2"],
+    song: require("../assets/乃木坂/18th-逃げ水.mp3"),
+  },
+  {
+    id: ["n-20-1", "n-20-2"],
+    song: require("../assets/乃木坂/20th-シンクロニシティ.mp3"),
+  },
+  {
+    id: ["n-23-1", "n-23-2"],
+    song: require("../assets/乃木坂/23th-Sing Out.mp3"),
+  },
+  {
+    id: ["n-24-1", "n-24-2"],
+    song: require("../assets/乃木坂/24th-夜明けまで強がらなくてもいい.mp3"),
+  },
+  {
+    id: ["n-25-1", "n-25-2"],
+    song: require("../assets/乃木坂/No25-幸せの保護色.mp3"),
+  },
+
+  {
+    id: ["s-1-1", "s-1-2"],
+    song: require("../assets/欅坂/1st-サイレントマジョリティー.mp3"),
+  },
+  {
+    id: ["s-3-2"],
+    song: require("../assets/欅坂/3rd-二人セゾン.mp3"),
+  },
+  {
+    id: ["s-4-1", "s-4-2", "s-4-3"],
+    song: require("../assets/欅坂/4th-不協和音.mp3"),
+  },
+  {
+    id: ["s-6-1"],
+    song: require("../assets/欅坂/6th-ガラスを割れ.mp3"),
+  },
+  {
+    id: ["s-8-1", "s-8-2", "s-8-3"],
+    song: require("../assets/欅坂/8th-黒い羊.mp3"),
+  },
+  {
+    id: ["s-9-1", "s-9-2"],
+    song: require("../assets/欅坂/Nobodys fault.mp3"),
+  },
+
+  {
+    id: ["h-1-1", "h-1-2", "h-1-3"],
+    song: require("../assets/日向坂/18th-キュン.mp3"),
+  },
+  {
+    id: ["h-2-1", "h-2-2", "h-2-3"],
+    song: require("../assets/日向坂/2nd-ドレミソラシド.mp3"),
+  },
+  {
+    id: ["h-3-1", "h-3-2"],
+    song: require("../assets/日向坂/3rd-こんなに好きになっちゃっていいの.mp3"),
+  },
+  {
+    id: ["h-4-1", "h-4-2", "h-4-3", "h-4-4"],
+    song: require("../assets/日向坂/4th-そんなことないよ.mp3"),
+  },
+
+  {
+    id: ["y-1-1", "y-1-2", "y-1-3"],
+    song: require("../assets/吉本坂/1st-泣かせてくれよ.mp3"),
+  },
+  {
+    id: ["y-2-1", "y-2-2", "y-2-3"],
+    song: require("../assets/吉本坂/2nd-今夜はええやんMusic Video  YOSHIMOTOZAKA46-Konyaha eeyan.mp3"),
+  },
+  {
+    id: ["y-3-1", "y-3-2", "y-3-3"],
+    song: require("../assets/吉本坂/3rd-不能ではいられない.mp3"),
+  },
+];
+
 const CollagePage: React.FC<{
   navigation: Record<string, unknown>;
   route: Record<string, unknown>;
@@ -17,12 +103,14 @@ const CollagePage: React.FC<{
   const [sound, setSound] = useState();
   const [isPlay, setIsPlay] = useState(false);
   const image = route.params?.img;
+  const responseData = route.params?.responseData;
+
+  //   const song = songs.find((t) => t.id.includes(responseData));
+  const song = require("../assets/23th-SingOut.mp3");
 
   async function playSound() {
     console.log("Loading Sound");
-    const { sound } = await Audio.Sound.createAsync(
-      require("../assets/23th-SingOut.mp3")
-    );
+    const { sound } = await Audio.Sound.createAsync(song);
     setSound(sound);
     setIsPlay(true);
 
