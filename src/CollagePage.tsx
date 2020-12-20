3b29f347011082e246f5808c60d7eee58aecdf74
ryu0114h <ryu0114h@gmail.com>
2020-12-20 10:34:30 +0900

    音楽流し

--- src/CollagePage.tsx
+++ src/CollagePage.tsx
@@ -1,7 +1,65 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+import { Audio } from "expo-av";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-const CollagePage: React.FC = () => {
-  return <View></View>;
+const CollagePage: React.FC<{
+  navigation: Record<string, unknown>;
+  route: Record<string, unknown>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}> = ({ navigation, route, setLoading }) => {
+  const [sound, setSound] = useState();
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/No25-幸せの保護色.mp3")
+    );
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    playSound();
+
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  return (
+    <View>
+      <TouchableOpacity style={styles.button} onPress={() => playSound()}>
+        {sound ? (
+          <Icon name="pause" size={20} />
+        ) : (
+          <Icon name="play" size={20} />
+        )}
+      </TouchableOpacity>
+    </View>
+  );
 };
 export default CollagePage;
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 15,
+    paddingVertical: 10,
+    margin: 5,
+    backgroundColor: "#EF5769",
+    width: Dimensions.get("screen").width * 0.9,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  whiteText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
